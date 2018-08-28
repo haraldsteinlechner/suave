070bbe5368441798dfc74add0a3187fee339628b
Harald Steinlechner <haraldsteinlechner@gmail.com>
2018-08-28 15:56:12 +0200

    bad test case

--- examples/WebSocket/Program.fs
+++ examples/WebSocket/Program.fs
@@ -1,4 +1,4 @@
-﻿
+
 open Suave
 open Suave.Http
 open Suave.Operators
@@ -23,39 +23,44 @@ let ws (webSocket : WebSocket) (context: HttpContext) =
 
     while loop do
       // the server will wait for a message to be received without blocking the thread
-      let! msg = webSocket.read()
+      let msg = webSocket.read() |> Async.RunSynchronously
 
       match msg with
-      // the message has type (Opcode * byte [] * bool)
-      //
-      // Opcode type:
-      //   type Opcode = Continuation | Text | Binary | Reserved | Close | Ping | Pong
-      //
-      // byte [] contains the actual message
-      //
-      // the last element is the FIN byte, explained later
-      | (Text, data, true) ->
-        // the message can be converted to a string
-        let str = UTF8.toString data
-        let response = sprintf "response to %s" str
+        | Choice1Of2 msg -> 
+      
+          match msg with
+          // the message has type (Opcode * byte [] * bool)
+          //
+          // Opcode type:
+          //   type Opcode = Continuation | Text | Binary | Reserved | Close | Ping | Pong
+          //
+          // byte [] contains the actual message
+          //
+          // the last element is the FIN byte, explained later
+          | (Text, data, true) ->
+            // the message can be converted to a string
+            let str = UTF8.toString data
+            let response = sprintf "response to %s" str
 
-        // the response needs to be converted to a ByteSegment
-        let byteResponse =
-          response
-          |> System.Text.Encoding.ASCII.GetBytes
-          |> ByteSegment
+            // the response needs to be converted to a ByteSegment
+            let byteResponse =
+              response
+              |> System.Text.Encoding.ASCII.GetBytes
+              |> ByteSegment
 
-        // the `send` function sends a message back to the client
-        do! webSocket.send Text byteResponse true
+            // the `send` function sends a message back to the client
+            webSocket.send Text byteResponse true |> Async.RunSynchronously
 
-      | (Close, _, _) ->
-        let emptyResponse = [||] |> ByteSegment
-        do! webSocket.send Close emptyResponse true
+          | (Close, _, _) ->
+            let emptyResponse = [||] |> ByteSegment
+            do! webSocket.send Close emptyResponse true
 
-        // after sending a Close message, stop the loop
-        loop <- false
+            // after sending a Close message, stop the loop
+            loop <- false
 
-      | _ -> ()
+          | _ -> ()
+        | _ ->
+          printfn "error"
     }
 
 /// An example of explictly fetching websocket errors and handling them in your codebase.
@@ -88,6 +93,32 @@ let app : WebPart =
 
 [<EntryPoint>]
 let main _ =
+  let mutable workers, ios = 0,0 
+  System.Threading.ThreadPool.GetMinThreads(&workers,&ios)
+  printfn "original min threads: %A, IO threads: %A" workers ios
+  System.Threading.ThreadPool.GetMaxThreads(&workers,&ios)
+  printfn "original max threads: %A, IO threads: %A" workers ios
+
+  // simulating 1 logical core...
+  System.Threading.ThreadPool.SetMinThreads(1,1) |> printfn "setting min threads worked: %A"
+  System.Threading.ThreadPool.SetMaxThreads(32767,1000) |> printfn "setting max threads worked: %A"
+
+  System.Threading.ThreadPool.GetMinThreads(&workers,&ios)
+  printfn "set min threads: %A, IO threads: %A" workers ios
+  System.Threading.ThreadPool.GetMaxThreads(&workers,&ios)
+  printfn "set max threads: %A, IO threads: %A" workers ios
+   
+  let sw = System.Diagnostics.Stopwatch.StartNew()
+  let observer =
+    async {
+      do! Async.SwitchToNewThread()
+      while true do
+        let mutable workers, ios = 0,0 
+        System.Threading.ThreadPool.GetAvailableThreads(&workers,&ios)
+        printfn "%03.2f available workers: %A, io's: %A" sw.Elapsed.TotalMilliseconds workers ios 
+        System.Threading.Thread.Sleep 50
+    } |> Async.Start
+
   startWebServer { defaultConfig with logger = Targets.create Verbose [||] } app
   0
 
